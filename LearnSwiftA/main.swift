13cb18ec933fed543380de5dc28e69f674723d68
Mercsjho <mercsjho@bogon>
2016-03-13 09:27:34 +0800

    Initial Commit

---
+++ LearnSwiftA/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LearnSwiftA
+//
+//  Created by Mercsjho on 16/3/13.
+//  Copyright © 2016年 Mercsjho. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
